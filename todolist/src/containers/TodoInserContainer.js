9cec6f023d34d76e6a6797a05a0344d9ffa78c4f
ijuhom <2014brand@naver.com>
2020-01-19 11:22:27 +0900

    리덕스 수정

--- todolist/src/containers/TodoInserContainer.js
+++ todolist/src/containers/TodoInserContainer.js
@@ -4,14 +4,12 @@ import { insert } from "../modules/todo";
 import TodoInsert from "../components/ReduxTodo/TodoInsert";
 
 const TodoInserContainer = () => {
-  const { todos } = useSelector(({ todo }) => ({
-    todos: todo.todos
-  }));
+
 
   const dispatch = useDispatch();
   const onInsert = useCallback(id => dispatch(insert(id)), [dispatch]);
 
-  return <TodoInsert insert={onInsert} todos={todos} />;
+  return <TodoInsert insert={onInsert}  />;
 };
 
 export default React.memo(TodoInserContainer);
